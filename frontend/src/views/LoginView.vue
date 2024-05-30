6253c145d98c8b85486ddc23557f38b990c9cb43
21DP2JFern <12djfernats@rvt.lv>
2024-05-30 10:30:11 +0300

    sadsad

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -29,7 +29,7 @@ export default{
       
       async login(){
         try{
-          const response = await axios.post(`/login`,{
+          const response = await axios.post(`/api/login`,{
             email:this.email, 
             password:this.password
           });
